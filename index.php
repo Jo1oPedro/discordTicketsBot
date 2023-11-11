21a92cf091aff5956684aad4a935599eb9c0bec6
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2023-11-11 04:56:55 -0300

    teste

--- index.php
+++ index.php
@@ -22,4 +22,17 @@ $discord->on('ready', function (Discord $discord) use ($result) {
     $channel = $discord->getChannel(CHANNEL_ID);
     // Envia uma mensagem para o canal.
     $channel->sendMessage($result->fetchAll(PDO::FETCH_ASSOC)[0]['id']);
+
+    // Listen for messages.
+    $discord->on(Event::MESSAGE_CREATE, function (Message $message, Discord $discord) {
+        if($message->author->bot) {
+            return;
+        }
+        $channel = $message->channel;
+        $channel->sendMessage("Testando resposta no bot: {$message->author->username}");
+        //$message->reply('Testando resposta no bot');
+
+        //echo "{$message->author->username}: {$message->content}", PHP_EOL;
+        // Note: MESSAGE_CONTENT intent must be enabled to get the content if the bot is not mentioned/DMed.
+    });
 });
