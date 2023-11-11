33ababa5fbfc39ba08280370bdac0326e8d66a1e
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2023-11-11 04:43:00 -0300

    Adiciona teste de envio de mensagem do discord

--- index.php
+++ index.php
@@ -3,7 +3,24 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use Discord\Bot\connect\Connect;
+use Discord\Discord;
+use Discord\Parts\Channel\Message;
+use Discord\WebSockets\Intents;
+use Discord\WebSockets\Event;
 
 $result = Connect::getInstance()->query("select * from personal_access_tokens");
 
-dd($result->fetchAll(PDO::FETCH_ASSOC)[0]);
+//dd($result->fetchAll(PDO::FETCH_ASSOC)[0]);
+$discord = new Discord([
+    'token' => TOKEN,
+    'intents' => Intents::getDefaultIntents()
+//      | Intents::MESSAGE_CONTENT, // Note: MESSAGE_CONTENT is privileged, see https://dis.gd/mcfaq
+]);
+
+$discord->on('ready', function (Discord $discord){
+    echo "Bot is ready!", PHP_EOL;
+
+    $channel = $discord->getChannel(CHANNEL_ID);
+    // Envia uma mensagem para o canal.
+    $channel->sendMessage('Mensagem a cada 5 segundos');
+});
