e81e692909c7d40ac3df3d143faa2e315133365a
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2023-11-11 04:40:37 -0300

    Altera tabela para personal_access_tokens

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Discord\Bot\connect\Connect;
 
-$result = Connect::getInstance()->query("select * from users");
+$result = Connect::getInstance()->query("select * from personal_access_tokens");
 
 dd($result->fetchAll(PDO::FETCH_ASSOC)[0]);
