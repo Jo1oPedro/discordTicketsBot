cb7d0ef692fd94a24742c5c423ae576a91bf75f4
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2023-11-11 04:35:10 -0300

    Minor changes

--- index.php
+++ index.php
@@ -5,7 +5,5 @@ require __DIR__ . '/vendor/autoload.php';
 use Discord\Bot\connect\Connect;
 
 $result = Connect::getInstance()->query("select * from users");
-var_dump(Connect::getInstance());
-dd($result);
 
 dd($result->fetchAll(PDO::FETCH_ASSOC)[0]);
